8f396e8553684dc3b9f90989e42e5f1b68ae5be3
Guram Savinov <gsavinov@rtech.ru>
2018-03-04 20:42:20 +0300

    step5

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,13 +2,19 @@
 
 /* Controllers */
 var phonecatApp = angular.module('phonecatApp', []);
-phonecatApp.controller('PhoneListCtrl', function ($scope) {
+phonecatApp.controller('PhoneListCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.title = 'Телефоны';
-    $scope.phones = [
-        {'name': 'Nexus S', 'snippet': 'Fast just got faster with Nexus S.', 'status': true, 'priority': 1},
-        {'name': 'Motorola XOOM™ with Wi-Fi', 'snippet': 'The Next, Next Generation tablet.', 'status': false, 'priority': 2},
-        {'name': 'MOTOROLA XOOM™', 'snippet': 'The Next, Next Generation tablet.', 'status': true, 'priority': 3}
-    ];
+    $http.get('phones/phones.json').success(function (data, status, headers, config) {
+        // console.log("data: " + data + " status: " + status + " headers: " + headers + " config: " + config);
+        $scope.phones = data;
+    }).error(function () {
+        
+    });
+    // $scope.phones = [
+    //     {'name': 'Nexus S', 'snippet': 'Fast just got faster with Nexus S.', 'status': true, 'priority': 1},
+    //     {'name': 'Motorola XOOM™ with Wi-Fi', 'snippet': 'The Next, Next Generation tablet.', 'status': false, 'priority': 2},
+    //     {'name': 'MOTOROLA XOOM™', 'snippet': 'The Next, Next Generation tablet.', 'status': true, 'priority': 3}
+    // ];
 
     /* Filters */
     $scope.today = new Date();
@@ -34,4 +40,4 @@ phonecatApp.controller('PhoneListCtrl', function ($scope) {
     $scope.isSortDown = function(fieldName) {
         return $scope.sortField === fieldName && $scope.reverse;
     };
-});
+}]);
