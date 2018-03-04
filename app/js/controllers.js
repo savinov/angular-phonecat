cef9e172557bb1ed22f409003f46505a1054e47a
Guram Savinov <gsavinov@rtech.ru>
2018-03-04 19:20:50 +0300

    step4

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,8 +5,33 @@ var phonecatApp = angular.module('phonecatApp', []);
 phonecatApp.controller('PhoneListCtrl', function ($scope) {
     $scope.title = 'Телефоны';
     $scope.phones = [
-        {'name': 'Nexus S', 'snippet': 'Fast just got faster with Nexus S.'},
-        {'name': 'Motorola XOOM™ with Wi-Fi', 'snippet': 'The Next, Next Generation tablet.'},
-        {'name': 'MOTOROLA XOOM™', 'snippet': 'The Next, Next Generation tablet.'}
-    ]
+        {'name': 'Nexus S', 'snippet': 'Fast just got faster with Nexus S.', 'status': true, 'priority': 1},
+        {'name': 'Motorola XOOM™ with Wi-Fi', 'snippet': 'The Next, Next Generation tablet.', 'status': false, 'priority': 2},
+        {'name': 'MOTOROLA XOOM™', 'snippet': 'The Next, Next Generation tablet.', 'status': true, 'priority': 3}
+    ];
+
+    /* Filters */
+    $scope.today = new Date();
+    $scope.doneAndFilter = function(phoneItem) {
+        return phoneItem.name && phoneItem.priority > 1 && phoneItem.status === true;
+    };
+
+    $scope.sortField = undefined;
+    $scope.reverse = false;
+    $scope.sort = function(fieldName) {
+        if ($scope.sortField === fieldName) {
+            $scope.reverse = !$scope.reverse;
+        } else {
+            $scope.sortField = fieldName;
+            $scope.reverse = false;
+        }
+    };
+
+    $scope.isSortUp = function(fieldName) {
+        return $scope.sortField === fieldName && !$scope.reverse;
+    };
+
+    $scope.isSortDown = function(fieldName) {
+        return $scope.sortField === fieldName && $scope.reverse;
+    };
 });
