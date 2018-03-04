14be434d0149ceceddcf08d789a3742a52b1ff35
Guram Savinov <gsavinov@rtech.ru>
2018-03-04 16:18:22 +0300

    step3

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,6 +2,11 @@
 
 /* Controllers */
 var phonecatApp = angular.module('phonecatApp', []);
-phonecatApp.controller('DemoCtrl', function ($scope) {
-    $scope.name = 'World!'
-})
+phonecatApp.controller('PhoneListCtrl', function ($scope) {
+    $scope.title = 'Телефоны';
+    $scope.phones = [
+        {'name': 'Nexus S', 'snippet': 'Fast just got faster with Nexus S.'},
+        {'name': 'Motorola XOOM™ with Wi-Fi', 'snippet': 'The Next, Next Generation tablet.'},
+        {'name': 'MOTOROLA XOOM™', 'snippet': 'The Next, Next Generation tablet.'}
+    ]
+});
